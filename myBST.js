1482aa795940b298f0d4b503b002616a9bbd8830
Reldalk <Reldalk@gmail.com>
2018-12-18 17:56:22 -0500

    copied a crazy algorithm online for kth largest

--- myBST.js
+++ myBST.js
@@ -28,14 +28,14 @@ class BinarySearchTree{
       if(node.left === null) 
         node.left = newNode; 
       else
-        this.insertNode(node.left, newNode);  
+        this._insertNode(node.left, newNode);  
     } 
     else
     { 
       if(node.right === null) 
         node.right = newNode; 
       else
-        this.insertNode(node.right,newNode); 
+        this._insertNode(node.right,newNode); 
     } 
   } 
 
@@ -79,13 +79,13 @@ class BinarySearchTree{
     } 
   
   } 
-  print(node){
+  inorder(node){
     if(node === null){
       return;
     }
+    this.inorder(node.right);
     console.log(node.data);
-    this.print(node.left);
-    this.print(node.right);
+    this.inorder(node.left);
   }
 
   findHeight(node){
@@ -97,13 +97,75 @@ class BinarySearchTree{
 
     return rightHeight > leftHeight ? rightHeight + 1 : leftHeight + 1;  
   }
+
+
+
+  isBST(node, previousNode){
+    if(node){
+      if(!this.isBST(node.left))
+        return false;
+      if(previousNode && node.data <= previousNode.data)
+        return false;
+      return this.isBST(node.right, node);
+    }
+    return true;
+  }
+
+  KthLargestUsingMorrisTraversal(root, k){
+    let curr = root;
+    let Klargest = null;
+
+    let count = 0;
+
+    while(curr !== null){
+      if(curr.right === null){
+        if(++count === k)
+          Klargest = curr;
+        curr = curr.left;
+      }
+
+      else{
+        let successor = curr.right;
+        while(successor.left !== null && successor.left !== curr)
+          successor = successor.left;
+        
+        if(successor.left === null){
+          successor.left = curr;
+          curr = curr.right;
+        }
+
+        else{
+          successor.left = null;
+          if(++count === k)
+            Klargest = curr;
+          curr = curr.left;
+        }
+      }
+    }
+    return Klargest;
+  }
+
 }
 
 const BST = new BinarySearchTree();
+
 BST.insert(5);
 BST.insert(10);
 BST.insert(20);
+BST.insert(4);
 BST.insert(3);
-BST.print(BST.root);
-console.log(BST.findHeight(BST.root));
-console.log(BST);
+BST.insert(30);
+BST.insert(40);
+BST.insert(6);
+
+/*
+BST.root = new Node(5);
+BST.root.left = new Node(10);
+BST.root.left.left = new Node(15);
+BST.root.right = new Node(2);
+*/
+//BST.inorder(BST.root);
+console.log(BST.KthLargestUsingMorrisTraversal(BST.root, 4));
+
+//console.log(BST.isBST(BST.root));
+
